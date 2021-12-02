57ac33ecf8d394f34b54e6cf24a52ed108825d13
PlaustralCL <plaustralcl@gmail.com>
2021-12-02 18:59:47 -0500

    Refactor day 1

--- day1/depth_analysis.rb
+++ day1/depth_analysis.rb
@@ -2,22 +2,21 @@
 
 # PartA
 # Given a file of depth measurements, countthe number of times a depth
-# measurement increases from the previous measurement.
+# measurement increases from the previous measurement. Answer: 1400
 
 depths = File.readlines("input.txt", chomp: true).map(&:to_i)
 
 def increasing_depths(depth_list)
-  (1..depth_list.length - 1).map { |index| depth_list[index] > depth_list[index - 1] }
+  (1..depth_list.length - 1).count { |index| depth_list[index] > depth_list[index - 1] }
 end
 
-# increasing_depths = (1..depths.length - 1).map { |index| depths[index] > depths[index - 1] }
 puts "Part A"
-puts "The number of single measurements that are larger than the previous measurement: #{increasing_depths(depths).count(true)}."
+puts "The number of single measurements that are larger than the previous measurement: #{increasing_depths(depths)}."
 puts
 
 # Part B
 # Consider sums of a three-measurement sliding window. How many sums are larger
-# than the previous sum?
+# than the previous sum? Answer: 1429
 windows = depths.each_cons(3).to_a.map(&:sum)
 puts "Part B"
-puts "The number of three measurement windows that are larger than the previous window: #{increasing_depths(windows).count(true)}."
+puts "The number of three measurement windows that are larger than the previous window: #{increasing_depths(windows)}."
