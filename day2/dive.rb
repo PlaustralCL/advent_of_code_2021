e928f23227a1bdacad4d9d41c02de023f9543185
PlaustralCL <plaustralcl@gmail.com>
2021-12-02 19:22:41 -0500

    Day 2 part B

--- day2/dive.rb
+++ day2/dive.rb
@@ -22,6 +22,41 @@ course.each do |direction, value|
   end
 end
 
+puts "Part A"
 puts "The horizontal position is: #{horizontal}."
 puts "The depth is: #{depth}."
 puts "Multipling #{horizontal} by #{depth} = #{horizontal * depth}."
+puts
+
+# Part B
+# In addition to horizontal position and depth, you'll also need to track a
+# third value, aim, which also starts at 0. The commands also mean something
+# entirely different than you first thought:
+# down x increases your aim by x units.
+# up x decreases your aim by x units.
+# forward x does two things:
+#   it increases your horizontal position by x units.
+#   it increases your depth by your aim multiplied by x.
+
+depth = 0
+horizontal = 0
+aim = 0
+
+course.each do |direction, value|
+  value = value.to_i
+  case direction
+  when "forward"
+    horizontal += value
+    depth += aim * value
+  when "down"
+    aim += value
+  when "up"
+    aim -= value
+  end
+end
+
+puts "Part B"
+puts "The horizontal position is: #{horizontal}."
+puts "The depth is: #{depth}."
+puts "Multipling #{horizontal} by #{depth} = #{horizontal * depth}."
+puts
